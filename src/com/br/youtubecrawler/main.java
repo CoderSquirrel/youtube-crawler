de71a23db14b51954a06de60370f8bb567ee4667
CoderSquirrel <saneschio@gmail.com>
2017-06-16 19:55:32 -0400

    lista total

--- src/com/br/youtubecrawler/main.java
+++ src/com/br/youtubecrawler/main.java
@@ -10,6 +10,9 @@ public class main {
 	private static List<Video> pagesToVisit = new ArrayList<Video>();
 
 	public static void main(String[] args) {
+		List<String> videoList = new ArrayList<String>();
+		List<String> accountsList = new ArrayList<String>();
+
 		Search search = new Search("Depois das Onze", 2);
 		List<Video> videos = search.doSearch();
 
@@ -20,13 +23,17 @@ public class main {
 					video.getComments().forEach(comment -> {
 						comment.getAutor().setLikedVideos(
 								LikedVideos.getLikedVideos(mountLikedURL(comment.getAutor().getChannel())));
-						comment.getAutor().setLikedVideos(FollowedAccounts
+						videoList.addAll(comment.getAutor().getLikedVideos());
+						comment.getAutor().setFollowedAccounts(FollowedAccounts
 								.getFollowedAccounts(mountFollowedURL(comment.getAutor().getChannel())));
+						accountsList.addAll(comment.getAutor().getFollowedAccounts());
 
 					});
 				}
 			});
 		}
+		
+		//accountsList e videoList contem todos os video e contas indicadas
 	}
 
 	private static String mountLikedURL(String channel) {
