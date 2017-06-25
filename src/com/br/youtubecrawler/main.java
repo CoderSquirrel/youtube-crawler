fc72f00557ab0be4afad481da5c6f511bd5c911d
sane <CoderSquirrel@users.noreply.github.com>
2017-06-24 20:57:09 -0400

    Update main.java

--- src/com/br/youtubecrawler/main.java
+++ src/com/br/youtubecrawler/main.java
@@ -1,8 +1,16 @@
 package com.br.youtubecrawler;
 
+import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.UnsupportedEncodingException;
+import java.io.Writer;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.br.crawler.domain.Comment;
 import com.br.crawler.domain.Video;
 
 public class main {
@@ -12,8 +20,8 @@ public class main {
 	public static void main(String[] args) {
 		List<String> videoList = new ArrayList<String>();
 		List<String> accountsList = new ArrayList<String>();
-
-		Search search = new Search("Depois das Onze", 2);
+		String toSearch = "Depois das Onze";
+		Search search = new Search(toSearch, 2);
 		List<Video> videos = search.doSearch();
 
 		if (videos != null && !videos.isEmpty()) {
@@ -31,9 +39,78 @@ public class main {
 					});
 				}
 			});
+			createArquivosGerais(toSearch, videoList, accountsList);
+			createFiles(videos);
 		}
-		
-		//accountsList e videoList contem todos os video e contas indicadas
+
+		// accountsList e videoList contem todos os video e contas indicadas
+	}
+
+	public static void createArquivosGerais(String search, List<String> videoList, List<String> accountsList) {
+		try {
+			Writer writer = new BufferedWriter(
+					new OutputStreamWriter(new FileOutputStream("result/"+search + "-Video.txt"), "utf-8"));
+			for (String s : videoList) {
+				writer.write(s + "\n");
+//				VGet v = new VGet(new URL(s), new File("result/"));
+//				System.out.println("Init Download");
+//	            v.download();
+//	            System.out.println("Finish Download");
+			}
+			writer.close();
+			writer = new BufferedWriter(
+					new OutputStreamWriter(new FileOutputStream("result/"+search + "-Accounts.txt"), "utf-8"));
+			for (String s : accountsList) {
+				writer.write(s);
+			}
+			writer.close();
+
+		} catch (
+
+		FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (UnsupportedEncodingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+	public static void createFiles(List<Video> videos) {
+		Writer writer = null, writer2 = null;
+
+		try {
+			for (Video video : videos) {
+				writer = new BufferedWriter(
+						new OutputStreamWriter(new FileOutputStream("result/"+video.getTitle() + "-Videos.txt"), "utf-8"));
+
+				writer2 = new BufferedWriter(
+						new OutputStreamWriter(new FileOutputStream("result/"+video.getTitle() + "-Accounts.txt"), "utf-8"));
+				if (video.getComments() != null) {
+					for (Comment comment : video.getComments()) {
+						for (String s : comment.getAutor().getFollowedAccounts()) {
+							writer2.write(s + "\n");
+						}
+
+						for (String s : comment.getAutor().getLikedVideos()) {
+							writer.write(s + "\n");
+						}
+					}
+				}
+				writer.close();
+				writer2.close();
+			}
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+		}
+
 	}
 
 	private static String mountLikedURL(String channel) {
